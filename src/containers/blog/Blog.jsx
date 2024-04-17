793ee8b34268bf7cda365bdc6b7a222d6dcb1689
Sekhar <sunkarisekhar36@gmail.com>
2024-04-17 12:58:23 +0530

    Added

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import "./blog.css";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
+import { Article } from "../../components";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient_text">A lot is Happening we are blogging it</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container-groupA">
+          <Article
+            imageUrl={blog01}
+            date="May 13,2004 "
+            title="Gpt3 and OpenAI is the future.Let us explore how it is ? "
+          />
+        </div>
+
+        <div className="gpt3__blog-container-groupB">
+          <Article
+            imageUrl={blog02}
+            date="May 13,2004 "
+            title="Gpt3 and OpenAI is the future.Let us explore how it is ? "
+          />
+          <Article
+            imageUrl={blog03}
+            date="May 13,2004 "
+            title="Gpt3 and OpenAI is the future.Let us explore how it is ? "
+          />
+          <Article
+            imageUrl={blog04}
+            date="May 13,2004 "
+            title="Gpt3 and OpenAI is the future.Let us explore how it is ? "
+          />
+          <Article
+            imageUrl={blog05}
+            date="May 13,2004 "
+            title="Gpt3 and OpenAI is the future.Let us explore how it is ? "
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,7 @@
+import blog01 from "../../assetsgpt/blog01.png";
+import blog02 from "../../assetsgpt/blog02.png";
+import blog03 from "../../assetsgpt/blog03.png";
+import blog04 from "../../assetsgpt/blog04.png";
+import blog05 from "../../assetsgpt/blog05.png";
+
+export { blog01, blog02, blog03, blog04, blog05 };
